cd7512dd4718bdb40e5e4fe3bef01471602d9b32
merdogan10 <mustafaerdogan1994@gmail.com>
2017-05-18 19:11:46 +0300

    Change view arrangements

--- app/src/main/java/com/tunahansalih/drawingexample/MainActivity.java
+++ app/src/main/java/com/tunahansalih/drawingexample/MainActivity.java
@@ -8,21 +8,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mukesh.DrawingView;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-    private Button mSaveButton, mPenButton, mEraserButton, mClearButton;
-    private Spinner mSpinner;
+        implements View.OnClickListener{
+    private Button mSaveButton, mPenButton, mClearButton, mEquals;
+    private TextView mCalculator;
     private DrawingView mDrawingView;
     private String selectedDataType = "0";
 
@@ -38,78 +36,46 @@ public class MainActivity extends AppCompatActivity
     private void setListeners() {
         mSaveButton.setOnClickListener(this);
         mPenButton.setOnClickListener(this);
-        mEraserButton.setOnClickListener(this);
         mClearButton.setOnClickListener(this);
-        mSpinner.setOnItemSelectedListener(this);
-
+        mEquals.setOnClickListener(this);
     }
 
     private void initializeUI() {
         mDrawingView = (DrawingView) findViewById(R.id.scratch_pad);
-        mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton = (Button) findViewById(R.id.set_number);
         mPenButton = (Button) findViewById(R.id.pen_button);
-        mEraserButton = (Button) findViewById(R.id.eraser_button);
         mClearButton = (Button) findViewById(R.id.clear_button);
-
-        //Spinner
-        mSpinner = (Spinner) findViewById(R.id.data_spinner);
-        List<String> dataTypes = new ArrayList<>();
-        dataTypes.add("0");
-        dataTypes.add("1");
-        dataTypes.add("2");
-        dataTypes.add("3");
-        dataTypes.add("4");
-        dataTypes.add("5");
-        dataTypes.add("6");
-        dataTypes.add("7");
-        dataTypes.add("8");
-        dataTypes.add("9");
-        dataTypes.add("+");
-        dataTypes.add("-");
-        dataTypes.add("*");
-        dataTypes.add("/");
-        dataTypes.add("=");
-        dataTypes.add(")");
-        dataTypes.add("(");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dataTypes);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(dataAdapter);
-
-
-
+        mEquals = (Button) findViewById(R.id.equals);
+        mCalculator = (TextView) findViewById(R.id.calculator);
     }
-
+    boolean isSet = false;
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.save_button:
-                File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/dataset/" + selectedDataType + "/");
-                try {
-                    if (dir.mkdirs()) {
-                        System.out.println("Folder created");
-                        Log.d("created", dir.getAbsolutePath());
-                    } else {
-                        System.out.println("Folder is not created");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                Long tsLong = System.currentTimeMillis() / 1000;
-                String ts = tsLong.toString();
-                mDrawingView.saveImage(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString() + "/dataset/" + selectedDataType, "/" + ts,
-                        Bitmap.CompressFormat.PNG, 100);
-                break;
             case R.id.pen_button:
-
+                mCalculator.setText("0");
                 mDrawingView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDrawingCanvas));
                 mDrawingView.initializePen();
                 mDrawingView.setPenColor(ContextCompat.getColor(this, R.color.colorPen));
                 mDrawingView.setPenSize(50);
                 break;
-            case R.id.eraser_button:
-                mDrawingView.initializeEraser();
-                mDrawingView.setEraserSize(100);
+            case R.id.set_number:
+                String addText = "5";
+                if(isSet == true){
+                    mCalculator.setText("0");
+                    isSet = false;
+                }
+                String text = String.valueOf(mCalculator.getText());
+                if(text.compareTo("0")==0){
+                    mCalculator.setText(addText);
+                } else {
+                    mCalculator.setText(text+addText);
+                }
+                mDrawingView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPen));
+                mDrawingView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDrawingCanvas));
+                mDrawingView.initializePen();
+                mDrawingView.setPenColor(ContextCompat.getColor(this, R.color.colorPen));
+                mDrawingView.setPenSize(50);
                 break;
             case R.id.clear_button:
                 mDrawingView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPen));
@@ -118,43 +84,11 @@ public class MainActivity extends AppCompatActivity
                 mDrawingView.setPenColor(ContextCompat.getColor(this, R.color.colorPen));
                 mDrawingView.setPenSize(50);
                 break;
+            case R.id.equals:
+                String myResult = "135";
+                isSet = true;
+                mCalculator.setText(myResult);
+                break;
         }
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String item = adapterView.getItemAtPosition(i).toString();
-        switch (item) {
-            case ("+"):
-                selectedDataType = "plus";
-                break;
-            case ("-"):
-                selectedDataType = "minus";
-                break;
-            case ("/"):
-                selectedDataType = "slash";
-                break;
-            case ("*"):
-                selectedDataType = "times";
-                break;
-            case ("("):
-                selectedDataType = "openingParanthesis";
-                break;
-            case (")"):
-                selectedDataType = "closingParanthesis";
-                break;
-            case ("="):
-                selectedDataType = "equals";
-            default:
-                selectedDataType = item;
-
-        }
-        selectedDataType = item;
-        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
     }
 }
